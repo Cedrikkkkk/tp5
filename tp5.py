8d041e32e1b848cfe3d595e0cf1f0743b32ca92e
Cedrikkkkk <desjardinsc@educ.college-montreal.qc.ca>
2026-02-13 13:48:56 -0500

    commit de fin

---
+++ tp5.py
@@ -0,0 +1,118 @@
+"""
+Starting Template
+
+Once you have learned how to use classes, you can begin your program with this
+template.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.starting_template
+"""
+import arcade
+
+WINDOW_WIDTH = 1480
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Starting Template"
+
+
+class GameView(arcade.View):
+    """
+    Main application class.
+
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+    def __init__(self):
+        super().__init__()
+        self.background_color = arcade.color.AMAZON
+        self.cercle_x = 50
+        self.cercle_y = 50
+        self.cercle_change_x = 7 # Nombre d'unité pour le déplacement sur l'axe des X
+        self.cercle_change_y = 7 #Nombre d'unité pour le déplacement sur l'axe des Y
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+
+    def reset(self):
+        """Reset the game to the initial state."""
+        # Do changes needed to restart the game here if you want to support that
+        pass
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        self.clear()
+        arcade.draw_circle_filled(self.cercle_x, self.cercle_y, 20, (255, 0, 0))
+
+    def on_update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+
+        self.cercle_x += self.cercle_change_x
+        self.cercle_y += self.cercle_change_y
+        if self.cercle_x < 20 :
+            self.cercle_change_x *= -1
+        if self.cercle_x > WINDOW_WIDTH - 20:
+            self.cercle_change_x *= -1
+        if self.cercle_y < 20:
+            self.cercle_change_y *= -1
+        if self.cercle_y > WINDOW_HEIGHT - 20:
+            self.cercle_change_y *= -1
+    def on_key_press(self, key, key_modifiers):
+        """-
+        Called whenever a key on the keyboard is pressed.
+
+        For a full list of keys, see:
+        https://api.arcade.academy/en/latest/arcade.key.html
+        """
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        """
+        Called whenever the user lets off a previously pressed key.
+        """
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
+
+
+def main():
+    """ Main function """
+    # Create a window class. This is what actually shows up on screen
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+
+    # Create and setup the GameView
+    game = GameView()
+
+    # Show GameView on screen
+    window.show_view(game)
+
+    # Start the arcade game loop
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
+    #aaaa
